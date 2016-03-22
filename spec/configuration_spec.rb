6b8de6c77936e550e016c39ed507438c2b4af522
Peter Graham <peterghm@gmail.com>
2016-03-22 00:37:45 -0400

    test that logger is set by default, but can be overridden

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,18 +1,22 @@
 describe Pheme do
   describe ".configure" do
-    let(:sns_client) { Aws::SNS::Client }
-    let(:sqs_client) { Aws::SQS::Client }
+    let(:sns_client) { double }
+    let(:sqs_client) { double }
+    let(:custom_logger) { double }
     it "sets global configuration" do
       expect(described_class.configuration.sns_client).to be_nil
       expect(described_class.configuration.sqs_client).to be_nil
+      expect(described_class.configuration.logger).to be_a(Logger)
 
       described_class.configure do |config|
         config.sns_client = sns_client
         config.sqs_client = sqs_client
+        config.logger = custom_logger
       end
 
       expect(described_class.configuration.sns_client).to eq(sns_client)
       expect(described_class.configuration.sqs_client).to eq(sqs_client)
+      expect(described_class.configuration.logger).to eq(custom_logger)
     end
   end
 end
