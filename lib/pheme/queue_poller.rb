5b595d4233c718344a96d5f4dcde929ee3d5a192
Marc Duez <marcduez@gmail.com>
2016-06-22 16:57:37 -0400

    Overrides common config with manual
    - Reverses merge to trump common config with user-defined config.

--- lib/pheme/queue_poller.rb
+++ lib/pheme/queue_poller.rb
@@ -6,11 +6,11 @@ module Pheme
       @queue_url = queue_url
       @queue_poller = Aws::SQS::QueuePoller.new(queue_url)
       @connection_pool_block = connection_pool_block
-      @poller_configuration = poller_configuration.merge({
+      @poller_configuration = {
         wait_time_seconds: 10, # amount of time a long polling receive call can wait for a mesage before receiving a empty response (which will trigger another polling request)
         idle_timeout: 20, # disconnects poller after 20 seconds of idle time
         skip_delete: true, # manually delete messages
-      })
+      }.merge(poller_configuration || {})
     end
 
     def poll
