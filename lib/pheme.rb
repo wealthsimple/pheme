c81f73dece8cbe20fa0fab6618f62587b55a9696
Justin Bull <justin@wealthsimple.com>
2017-09-26 17:27:23 -0400

    Support and test AWS SDK 2 & 3

--- lib/pheme.rb
+++ lib/pheme.rb
@@ -1,6 +1,12 @@
 require 'active_support/all'
 require 'recursive-open-struct'
-require 'aws-sdk'
+require 'aws-sdk-core'
+begin
+  require 'aws-sdk-sqs' unless defined?(Aws::SQS)
+  require 'aws-sdk-sns' unless defined?(Aws::SNS)
+rescue LoadError
+  fail "AWS SDK 3 requires aws-sdk-sqs and aws-sdk-sqs to be installed separately. Please add gem 'aws-sdk-sqs' and 'aws-sdk-sns' to your Gemfile"
+end
 require 'securerandom'
 require 'smarter_csv'
 
