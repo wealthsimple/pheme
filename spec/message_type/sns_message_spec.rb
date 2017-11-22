9295b40bb61ffb94b4bbb5803a7af6f08a913c34
Marco Costa <mmarcottulio@gmail.com>
2017-11-22 17:57:05 -0500

    Fix test

--- spec/message_type/sns_message_spec.rb
+++ spec/message_type/sns_message_spec.rb
@@ -7,6 +7,19 @@ describe Pheme::MessageType::SnsMessage do
 
   subject { SnsMessage::Fixture.new }
 
+  let(:poller) do
+    poller = double
+    allow(poller).to receive(:poll).with(kind_of(Hash))
+    allow(poller).to receive(:parse_message)
+    allow(poller).to receive(:before_request)
+    poller
+  end
+  
+  before(:each) do
+    use_default_configuration!
+    allow(Aws::SQS::QueuePoller).to receive(:new) { poller }
+  end
+
   describe "#parse_message" do
     context "with JSON message" do
       let!(:message) { OpenStruct.new({ body: '{"Message":"{\"test\":\"test\"}"}' }) }
