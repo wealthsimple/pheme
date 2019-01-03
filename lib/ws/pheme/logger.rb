94c79078f18546d94609aba0e1899c9a966f04df
Danilo Cabello <cabello@users.noreply.github.com>
2019-01-03 14:17:24 -0500

    Don't modify tag for now because it could be used by log alerts.

--- lib/ws/pheme/logger.rb
+++ lib/ws/pheme/logger.rb
@@ -1,7 +1,7 @@
 module Ws::Pheme
   def self.log(method, text)
     @logger ||= ActiveSupport::TaggedLogging.new(configuration.logger)
-    @tag ||= "ws-pheme_#{SecureRandom.uuid}"
+    @tag ||= "pheme_#{SecureRandom.uuid}"
     @logger.tagged(@tag) { @logger.send(method, text) }
   end
 
