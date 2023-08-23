dff0d8dec95addc5c29697c9c313c02dd93c8d21
Teresa <loteresa2@gmail.com>
2023-08-23 09:24:14 -0400

    fix failing test

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -27,6 +27,7 @@ describe Pheme do
     skip('already on main branch, no need to compare versions') if is_main_branch
 
     head_version = get_version(git, 'HEAD')
+    puts "head_version " + head_version
     raise 'no version.rb file found on the current branch' if head_version.nil?
 
     expect(Gem::Version.new(head_version)).to be > Gem::Version.new(main_version)
